214861539ea43503c5a7250b1f34cb287a6f214d
tg322 <tom.shaun.grennan@gmail.com>
2025-05-30 15:26:10 +0100

    Modal content container component
    Created: Modal content container for semantics and modularity

---
+++ src/webparts/boscoHeroSection/Modal/ModalContent.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { IModalContentProps } from './IModalProps';
+
+function ModalContent(props:IModalContentProps){
+
+    const{
+        children
+    } = props
+
+    return(
+        <div style={{display:'flex', width:'100%', height:'100%', flexDirection:'column'}}>
+            {children}
+        </div>
+    );
+}
+export default ModalContent
