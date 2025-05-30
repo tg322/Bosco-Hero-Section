b77384e294edc69e036d49ffe4689389be23b05c
tg322 <tom.shaun.grennan@gmail.com>
2025-05-30 15:05:00 +0100

    Tidying up and improving logic
    Created: CalendarModal to contain Modal Logic

---
+++ src/webparts/boscoHeroSection/components/Calendar/CalendarModal.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import Modal from '../../Modal/Modal';
+import { CalendarLtrRegular } from '@fluentui/react-icons';
+import { useCalendarDispatchContext, useCalendarStateContext } from './CalendarContext';
+import ModalContent from '../../Modal/ModalContent';
+import { shortMonthStrings } from '../IBoscoHeroSectionProps';
+import ModalActionBar from '../../Modal/ModalActionBar';
+import { Button } from '@fluentui/react-components';
+
+function CalendarModal(){
+
+    const{calendarDispatch} = useCalendarDispatchContext();
+    const{calendarState} = useCalendarStateContext();
+
+    function close(){
+        calendarDispatch({type:'TOGGLE_MODAL',payload:false})
+        calendarDispatch({type:'RESET_CALENDAR_EVENT'})
+    }
+
+    if(calendarState.calendar){
+        return(
+            <Modal title='Event' size='small' titleIcon={<CalendarLtrRegular/>} open={calendarState.showModal} close={close}>
+                <ModalContent>
+                    <h2 style={{margin:'0px'}}>{shortMonthStrings[calendarState.calendar.startDate.getMonth()]}</h2>
+                    <h2 style={{margin:'0px'}}>{calendarState.calendar.startDate.getDate()}</h2>
+                    <h3 style={{margin:'0px', fontWeight:'400'}}>{calendarState.calendar.subject}</h3>
+                </ModalContent>
+                <ModalActionBar>
+                    <Button appearance='primary' shape='rounded' onClick={close}> Close</Button>
+                </ModalActionBar>
+            </Modal>
+        );
+    }else{
+        return(
+            <></>
+        );
+    }
+}
+
+export default CalendarModal
