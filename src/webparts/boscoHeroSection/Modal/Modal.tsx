7add5a170ef01b89982eb96a8a0bb9ef3c0f174e
tg322 <tom.shaun.grennan@gmail.com>
2025-05-30 15:15:53 +0100

    Large restructure to improve modularity
    Added: css class names
    Added: Modular modal components
    Added: Size prop logic
    Added: Type checking on children prop
    Changed: Close is now passed as a prop from parent
    Changed: IModalProps to reflect logic and prop changes

--- src/webparts/boscoHeroSection/Modal/Modal.tsx
+++ src/webparts/boscoHeroSection/Modal/Modal.tsx
@@ -1,50 +1,49 @@
 import * as React from 'react';
 import styles from './Modal.module.scss';
 import { DismissRegular } from '@fluentui/react-icons';
-import type { FluentIconsProps } from '@fluentui/react-icons';
-import { type ReactElement}  from 'react';
-import { useCalendarDispatchContext } from '../components/Calendar/CalendarContext';
+import ModalTitle from './ModalTitle';
+import { IModalActionBarProps, IModalContentProps, ModalSize } from './IModalProps';
+import ModalContent from './ModalContent';
+import ModalActionBar from './ModalActionBar';
 
-type TitleIcon = 
-  ReactElement<FluentIconsProps>
+type ModalContentComponent = React.ReactElement<IModalContentProps, typeof ModalContent>;
+type ModalActionBarComponent = React.ReactElement<IModalActionBarProps, typeof ModalActionBar>
+
+type ModalChildren = ModalContentComponent | [ModalContentComponent , ModalActionBarComponent] 
+
+type ModalSize = 'small' | 'medium' | 'large'
 
 interface IModalProps{
-  title:string;
-  titleIcon?:TitleIcon;
   open:boolean;
-  children:React.ReactNode;
+  children:ModalChildren;
+  title:string;
+  close:()=>void;
+  titleIcon?: React.ReactNode;
+  size:ModalSize;
 }
 
 function Modal(props: IModalProps){
 
   const{
-    title,
-    titleIcon,
     open,
-    children
+    children,
+    title,
+    close,
+    titleIcon,
+    size
   } = props
-
-  const{calendarDispatch} = useCalendarDispatchContext();
-
-  function close(){
-    calendarDispatch({type:'TOGGLE_MODAL',payload:false})
-    calendarDispatch({type:'RESET_CALENDAR_EVENT'})
-  }
 
   if(open){
     return(
       <div className={`${styles.modal}`}>
-          <div className={`${styles.modalContainer}`}>
+          <div className={`${styles.modalContainer}`} style={{minWidth:`${ModalSize[size].min}px`, maxWidth:`${ModalSize[size].max}px`}}>
               <div className={`${styles.modalTopBar}`}>
-                <div className={`${styles.modalTitle}`}>
-                  {titleIcon}
-                  <h1>{title}</h1>
-                </div>
+                <ModalTitle title={title} titleIcon={titleIcon}/>
                 <div className={`${styles.modalCloseButton}`} onClick={close}>
                   <DismissRegular className={`${styles.modalCloseButtonIcon}`}/>
                 </div>
               </div>
-              <div style={{display:'flex', flexDirection:'column'}}>
+              <div className={`${styles.modalContentWrapper}`}>
                 {children}
               </div>
           </div>
