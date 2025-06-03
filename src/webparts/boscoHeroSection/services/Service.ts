f51e06ffc6e0a30eae0cbcb377a1371d627a1f4c
tg322 <tom.shaun.grennan@gmail.com>
2025-06-03 16:01:20 +0100

    Comments and method descriptions.

--- src/webparts/boscoHeroSection/services/Service.ts
+++ src/webparts/boscoHeroSection/services/Service.ts
@@ -46,6 +46,16 @@ export class Service{
 
     }
 
+    /**
+     * Reserved for BackgroundUpload Custom Property.
+     * Checks for the existance of the main folder where background images will be stored, if no folder is found, one is created.
+     *
+     *
+     * @returns Array e.g {success:true, message: 'Data retrieved.', data:{data}}
+     * 
+     * @beta
+     */
+
     public async checkMainFolder(){
 
         const mainFolder:BuildResponseType = await this.dataHandler.checkFolderExistsInSP(this.context, window.location.origin, 'Shared Documents', this.context.manifest.alias);
@@ -66,6 +76,20 @@ export class Service{
 
     }
 
+    /**
+     * Gets current user information.
+     *
+     * @remarks
+     * This method uses the SharePoint Graph API.
+     * 
+     *
+     * @param filters - (Optional) Query filters.
+     *
+     * @returns Array e.g {success:true, message: 'Data retrieved.', data:{data}}
+     *
+     * @beta
+     */
+
     public async getMeInformation(filters?:string){
 
         const getMeInformationResponse:BuildResponseType = await this.graphHandler.getMe(filters);
@@ -85,6 +109,17 @@ export class Service{
         return preparedUserInfoResponse
 
     }
+
+    /**
+     * Gets news.
+     *
+     * @remarks
+     * This method uses the SharePoint Graph API.
+     *
+     * @returns Array e.g {success:true, message: 'Data retrieved.', data:{data}}
+     *
+     * @beta
+     */
 
     public async getNews(){
         const getPagesResponse:BuildResponseType = await this.graphHandler.getSitePages(this.context.pageContext.site.id, "$orderby=lastModifiedDateTime desc&$top=20&$filter=name ne 'Home.aspx' and name ne 'Our-Schools.aspx' and name ne 'St-Wilfrid''s-Staff.aspx'");
@@ -132,10 +167,27 @@ export class Service{
 
     //1d3d56b6-30ac-4a22-8586-c9537b2b7cea
 
+    /**
+     * Gets calendar events.
+     *
+     * @remarks
+     * This method uses the SharePoint Graph API.
+     *
+     * @returns Array e.g {success:true, message: 'Data retrieved.', data:{data}}
+     *
+     * @beta
+     */
+
     public async getCalendar(){
+
+        //Date range to fetch calendar events between.
         const today = new Date();
         const newDate = new Date(new Date(today.toDateString()).setMonth(today.getMonth() + 1));
+
+        //get calendar events
+        //Todo: Make group id a property in property pane (Fetch all public cal groups name and id, show in dropdown list, allow user to select, input group id in here from cal context)
         const getCalendarItemsResponse:BuildResponseType = await this.graphHandler.getCalendarItems('1d3d56b6-30ac-4a22-8586-c9537b2b7cea', today.toDateString(), newDate.toDateString(), 6);
+
 
         if(!getCalendarItemsResponse.success){
             return getCalendarItemsResponse
@@ -145,6 +197,20 @@ export class Service{
 
         return prepareCalendarEventsResponse
     }
+
+    /**
+     * Gets news author details by email.
+     *
+     * @remarks
+     * This method uses the SharePoint Graph API.
+     * 
+     *
+     * @param email - The news author's email.
+     *
+     * @returns Array e.g {success:true, message: 'Data retrieved.', data:{data}}
+     *
+     * @beta
+     */
 
     public async getNewsAuthorDetails(email:string){
         const getUsersResponse:BuildResponseType = await this.graphHandler.getUsers(`$filter=mail eq '${email}'&$select=id,displayName,department,companyName,jobTitle,officeLocation,mail,businessPhones`);
