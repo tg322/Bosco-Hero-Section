046e76f9bf5653a0fccb2778e14dbe5411926bfb
tg322 <tom.shaun.grennan@gmail.com>
2025-06-03 15:47:05 +0100

    No file changes -> Changes to dir structure

---
+++ src/webparts/boscoHeroSection/components/Calendar/components/CalendarItem.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { ICalendarItemProps, shortMonthStrings } from '../../IBoscoHeroSectionProps';
+import styles from '../../BoscoHeroSection.module.scss';
+import { useCalendarDispatchContext } from '../CalendarContext';
+
+function CalendarItem(props:ICalendarItemProps){
+    const{
+        calendarEvent
+    } = props
+
+    const{calendarDispatch} = useCalendarDispatchContext();
+
+    function openModal(){
+        calendarDispatch({type:'SET_CALENDAR_EVENT', payload:calendarEvent})
+        calendarDispatch({type:'TOGGLE_MODAL', payload:true})
+    }
+
+    return(
+        <div className={`${styles.boscoCalendarItemContainer}`} onClick={openModal}>
+            <div className={`${styles.boscoCalendarItemDateContainer}`}>
+                <h3 className={`${styles.boscoCalendarItemDate}`}>{calendarEvent.startDate.getDate()} {shortMonthStrings[calendarEvent.startDate.getMonth()]}</h3>
+            </div>
+            
+            <p className={`${styles.boscoCalendarItemSubject}`}>{calendarEvent.subject}</p>
+            <p className={`${styles.boscoCalendarItemStartEndTime}`}>{calendarEvent.startTime} to {calendarEvent.endTime}</p>
+        </div>
+    );
+}
+
+export default CalendarItem
