ee9da91808e26bf545f00771489a9711449d5bdb
tg322 <tom.shaun.grennan@gmail.com>
2025-06-09 15:46:57 +0100

    Temp, will remove later.

---
+++ src/webparts/boscoHeroSection/components/News/components/NewsAuthorToolTip.tsx
@@ -0,0 +1,44 @@
+// import * as React from 'react';
+// import { ISDUserProps } from '../../staffToolTip/IStaffDirectoryProps';
+// import { BuildResponseType } from '../../IBoscoHeroSectionProps';
+// import { useEffect, useState } from 'react';
+// import { useServiceContext } from '../../ServiceContext';
+// import UserToolTip from '../../userToolTip/UserToolTip';
+
+// interface INewsAuthorToolTipProps{
+//     authorEmail:string;
+//     hover:boolean;
+//     overflowLeft:number;
+// }
+
+// function NewsAuthorToolTip(props:INewsAuthorToolTipProps){
+
+//     const{
+//         authorEmail,
+//         hover,
+//         overflowLeft
+//     } = props
+
+
+
+//     console.log(overflowLeft);
+
+    
+
+//     useEffect(()=>{
+//         getAuthorDetails();
+//     },[hover])
+
+//     if(authorDetails && hover){
+//         return(
+            
+//         );
+//     }else{
+//         return(
+//             <></>
+//         );
+//     }
+    
+// }
+
+// export default NewsAuthorToolTip
