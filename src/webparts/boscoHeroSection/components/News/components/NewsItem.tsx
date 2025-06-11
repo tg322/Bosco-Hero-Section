597f5480674d4be034450056bfb41b63f8127925
tg322 <tom.shaun.grennan@gmail.com>
2025-06-11 12:32:01 +0100

    Changed imports and usage to use the new UserCard.

--- src/webparts/boscoHeroSection/components/News/components/NewsItem.tsx
+++ src/webparts/boscoHeroSection/components/News/components/NewsItem.tsx
@@ -2,11 +2,10 @@ import * as React from 'react';
 import styles from '../../BoscoHeroSection.module.scss';
 import { BuildResponseType, INewsItemProps, shortMonthStrings } from '../../IBoscoHeroSectionProps';
 import { useEffect, useRef, useState } from 'react';
-import { ISDUserProps } from '../../userToolTip/components/IStaffDirectoryProps';
 import { useServiceContext } from '../../ServiceContext';
-// import UserToolTip from '../../userToolTip/UserToolTip';
-import newsFallback from '../../../assets/news-fallback-thumbnail.png'
-import UserToolTip from '../../userToolTip/UserToolTip';
+import newsFallback from '../../../assets/news-fallback-thumbnail.png';
+import UserCard from '../../userToolTip/UserCard';
+import { IUserCardUserProps } from '../../userToolTip/components/IUserCardProps';
 
 function NewsItem(props:INewsItemProps){
 
@@ -18,7 +17,7 @@ function NewsItem(props:INewsItemProps){
 
     const hoverElmRef = useRef<HTMLDivElement>(null);
 
-    const[authorDetails, setAuthorDetails] = useState<ISDUserProps | null>(null);
+    const[authorDetails, setAuthorDetails] = useState<IUserCardUserProps | null>(null);
 
     const{svc} = useServiceContext();
 
@@ -41,7 +40,7 @@ function NewsItem(props:INewsItemProps){
 
     return(
     <div className={`${styles.boscoNewsItemWrapper}`} >
-        <div className={`${styles.boscoNewsItemContainer}`} onClick={handleNewsClick}>
+        <div className={`${styles.boscoNewsItemContainer}`} onClick={handleNewsClick} style={{cursor:'pointer'}}>
             <div id='newsThumbnailContainer' style={{height:'100%', width:'100px', display:'flex', backgroundImage:`url(${newsItem.thumbnail ? newsItem.thumbnail : newsFallback})`, backgroundPosition:'center', backgroundSize:'cover', borderRadius:'6px 0px 0px 6px'}}>
 
             </div>
@@ -49,9 +48,9 @@ function NewsItem(props:INewsItemProps){
             <div id='newsContentContainer' style={{display:'flex', flexDirection:'column', padding:'15px', boxSizing:'border-box', gap:'10px'}}>
                 <h3 style={{margin:'0px'}}>{newsItem.title}</h3>
 
-                <div ref={hoverElmRef} id='newsDetails' style={{width:'100%', display:'flex', flexDirection:'column', position:'relative'}} onMouseLeave={()=>setHover(true)}>
+                <div ref={hoverElmRef} id='newsDetails' style={{width:'100%', display:'flex', flexDirection:'column', position:'relative'}} onMouseLeave={()=>setHover(false)}>
                     <span style={{fontSize:'12px'}} onMouseOver={()=>setHover(true)}>{newsItem.authorName} {shortMonthStrings[newsItem.created.getMonth()]} {newsItem.created.getDate()}</span>
-                    {hover && authorDetails && <UserToolTip user={authorDetails} hoverElmRef={hoverElmRef} hover={hover}/>}
+                    {hover && authorDetails && <UserCard user={authorDetails} hoverElmRef={hoverElmRef} hover={hover}/>}
                 </div>
             </div>
         </div>
